87bd9676467c102b3c454e20e1d79b6d7624885c
David-YuWei <wyu2@albany.edu>
2017-09-19 17:46:52 -0400

    Implemented properties tab

--- client/src/render.js
+++ client/src/render.js
@@ -4,9 +4,16 @@ import vs from './values-table/';
 import tabs from './tabs/'
 
 export default function render(keyword, option, items){
-  let trsHtml = trs.render();
-  let psHtml = ps.render();
-  let vsHtml = vs.render(items);
-  let html = tabs(trsHtml, psHtml, vsHtml);
+  let html = "";
+  if(items.length !== 0){
+  	let trsHtml = trs.render(items);
+	let psHtml = ps.render(items);
+	let vsHtml = vs.render(items);
+	html = tabs(trsHtml, psHtml, vsHtml);
+  }
+  else{
+  	html = '<div class="info">No result found for keyword: '+keyword+'</div>';
+  }
+  
   $("#root").html(html);
 }
--- client/src/search-bar/view.js
+++ client/src/search-bar/view.js
@@ -1,2 +1,7 @@
 
-let tmpl = '<div></div>';
+let tmpl = '{{for options}}<div>'
+			+'<span style="width:96%;float:left;">{{:id}}</span>'
+			+'<label>{{:type}}</label>'
+			+'</div>{{/for}}';
+
+export default tmpl;
