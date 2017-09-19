be6b168e763bcc22591aee1ea4865cf87c2c5ddf
David-YuWei <wyu2@albany.edu>
2017-09-19 12:34:30 -0400

    format the front-end code

--- client/src/render.js
+++ client/src/render.js
@@ -1,5 +1,5 @@
 import vs from './values-table/';
-import navTabs from './tabs'
+import navTabs from './tabs/'
 
 export default function render(keyword, option, items){
   let vsHtml = vs.render(items);
