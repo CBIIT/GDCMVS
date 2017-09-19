e54498c0613bee7189caf5a0552166f46529bc2b
Francisco Miranda <fmanuelos@gmail.com>
2017-09-19 15:41:21 -0400

    Added gdc header and footer

---
+++ client/assets/js/wa_gdc_post.js
@@ -0,0 +1,23 @@
+﻿/*
+* Web Analytics functions snippet for GDC sites
+*/
+s.linkInternalFilters = wa_linkInternalFilters;
+
+s.channel = wa_channel;
+s.events = 'event1';
+var s_code = s.t(); if (s_code) document.write(s_code);
+
+var NCIAnalytics = {
+    
+    // Track sitewide searches
+    SiteSearch: function(sender) {
+        var s = s_gi(s_account);
+        s.linkTrackVars = 'channel,prop14,eVar14,events';
+        s.linkTrackEvents = 'event2';
+        s.prop14 = sender.elements[0].value;
+        s.eVar14 = sender.elements[0].value;
+        s.channel = wa_channel;
+        s.events = 'event2';
+        s.tl(this, 'o', wa_search_function_name);
+    }
+};
