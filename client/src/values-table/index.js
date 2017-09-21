5857a76bdf22bb133ec2c65af6f8e335a0d3119e
David-YuWei <wyu2@albany.edu>
2017-09-21 16:23:34 -0400

    Implemented the features in the values tab.

--- client/src/values-table/index.js
+++ client/src/values-table/index.js
@@ -9,82 +9,142 @@ const func = {
 	  	let hl = item.highlight;
 	  	let source = item._source;
 	  	let dict_enum_n = {};
-		let dict_enum_s ={};
-		let value = {};
-		value._id = item._id;
-		value.category = source.category;
-		value.node =  source.node;
-		value.name = source.name;
-	  	if(("enum.n" in hl || "enum.n.have" in hl || "enum.s" in hl || "enum.s.have" in hl || "cde_pv.ss.s" in hl )){
-			let enum_n = ("enum.n" in hl) || ("enum.n.have" in hl) ? hl["enum.n"] || hl["enum.n.have"] : [];
-			let enum_s = ("enum.s" in hl) || ("enum.s.have" in hl) ? hl['enum.s'] || hl["enum.s.have"] : [];
-			let cde_pv_s = ("cde_pv.ss.s" in hl) ? hl['cde_pv.ss.s'] : 'No Value';
-			enum_n.forEach(function(n){
-				let tmp = n.replace(/<b>/g,"").replace(/<\/b>/g, "");
-				dict_enum_n[tmp] = n;
-			});
-			enum_s.forEach(function(s){
-				let tmp = s.replace(/<b>/g,"").replace(/<\/b>/g, "");
-				dict_enum_s[tmp] = s;
-			});
-		}
-		value.vs = [];
-
-		if(source.enum){
-
+		let dict_enum_s = {};
+		let dict_cde_s = {};
+		//each row in the values tab will be put into values
+		let row = {};
+		row.category = source.category;
+		row.node =  source.node;
+		row.name = source.name;
+		row.local = source.enum == undefined ? false : true;
+		row.syn = false;
+		if(source.enum !== undefined){
+			//check if synonyms exists
 			source.enum.forEach(function(em){
-				//check if em.n exists in the dict_enum_n
-				let vs = {}
-				if(em.n in dict_enum_n){
+				if(row.syn) return;
 
-					vs.n = dict_enum_n[em.n];
-					vs.n_c = em.n_c;
-					vs.syn = true;
-					vs.s = [];
-
-					if(em.s){
-						
-						em.s.forEach(function(s){
-							if(s in dict_enum_s){
-								vs.s.push(dict_enum_s[s])
-							}
-							else{
-								vs.s.push(s);
-							}
-						});				
-					}
-
-					value.vs.push(vs);
-
+				if(em.n_c !== undefined){
+					row.syn = true;
 				}
-				else{
-					if(em.s){
-						let tmp = [];
-						let exist = false;
-						em.s.forEach(function(s){
-							if(s in dict_enum_s){
+			});	
+		}
+		row.ref = source.name +"@" +source.node +"@" + source.category;
+		row.cdeId = source.cde_id !== undefined ? source.cde_id : "";
+		row.cdeLen = source.cde_pv == undefined || source.cde_pv.length == 0 ? false : true;
+		//value informations in the subtable
+		row.vs = [];
+	  	let enum_n = ("enum.n" in hl) || ("enum.n.have" in hl) ? hl["enum.n"] || hl["enum.n.have"] : [];
+		let enum_s = ("enum.s" in hl) || ("enum.s.have" in hl) ? hl['enum.s'] || hl["enum.s.have"] : [];
+		let cde_s = ("cde_pv.ss.s" in hl) || ("cde_pv.ss.s.have" in hl) ? hl["cde_pv.ss.s"] || hl["cde_pv.ss.s.have"] : [];
+		enum_n.forEach(function(n){
+			let tmp = n.replace(/<b>/g,"").replace(/<\/b>/g, "");
+			dict_enum_n[tmp] = n;
+		});
+		enum_s.forEach(function(s){
+			let tmp = s.replace(/<b>/g,"").replace(/<\/b>/g, "");
+			dict_enum_s[tmp] = s;
+		});
+		cde_s.forEach(function(ps){
+			let tmp = ps.replace(/<b>/g,"").replace(/<\/b>/g, "");
+			dict_cde_s[tmp] = ps;
+		});
+
+		//check if there are any matches in the cde synonyms
+		let matched_pv = {};
+		if(source.cde_pv !== undefined && source.cde_pv.length > 0){
+			source.cde_pv.forEach(function(pv){
+				let exist = false;
+				let tmp_ss = [];
+				if(pv.ss !== undefined && pv.ss.length > 0){
+					pv.ss.forEach(function(ss){
+						let tmp_s = []
+						ss.s.forEach(function(s){
+							if(s in dict_cde_s){
 								exist = true;
-								tmp.push(dict_enum_s[s]);
+								tmp_s.push(dict_cde_s[s])
 							}
 							else{
-								tmp.push(s);
+								tmp_s.push(s);
 							}
 						});
-						if(exist){
-							value.vs.push({n:em.n, n_c: em.n_c, syn: true,s:tmp});
-						}					
-					}
+						tmp_ss.push({c: ss.c, s: tmp_s});
+					});
 				}
-
+				if(exist){
+					matched_pv[pv.n.toLowerCase()] = tmp_ss;
+				}
+			});
+		}
+		
+		if(source.enum){
+			source.enum.forEach(function(em){
+				//check if there are any matches in local synonyms
+				let exist = false;
+				let tmp_s = [];
+				if(em.s){
+					em.s.forEach(function(s){
+						if(s in dict_enum_s){
+							exist = true;
+							tmp_s.push(dict_enum_s[s])
+						}
+						else{
+							tmp_s.push(s);
+						}
+					});
+				}
+				//value to be put into the subtable
+				let v = {};
+				if(exist){
+					//check if there is a match with the value name
+					if(em.n in dict_enum_n){
+						v.n = dict_enum_n[em.n];
+					}
+					else{
+						v.n = em.n;
+					}
+					v.ref = row.ref;
+					v.n_c = em.n_c;
+					v.s = tmp_s;
+				}
+				else{
+					if(em.n in dict_enum_n){
+						v.n = dict_enum_n[em.n];
+						v.ref = row.ref;
+						v.n_c = em.n_c;
+						v.s = em.s;
+					}
+					
+				}
+				//check if there are any matched cde_pvs can connect to this value
+				if(v.n !== undefined){
+					let lc = em.n.toLowerCase();
+					if(lc in matched_pv){
+						v.cde_s = matched_pv[lc];
+						delete matched_pv[lc];
+					}
+					else{
+						v.cde_s = [];
+					}
+					row.vs.push(v);
+				}
+				
 			});
 		}
 
-		values.push(value);
+		//add the rest of the matched cde_pvs to the subtables
+		for(let idx in matched_pv){
+			let v = {};
+			v.n = "See All Values";
+			v.ref = row.ref;
+			v.n_c = "";
+			v.s = [];
+			v.cde_s = matched_pv[idx];
+			row.vs.push(v);
+		}
 
-		
+		values.push(row);
 	});
 	console.log(values);
-
     let html = $.templates(tmpl).render({values: values});
 
     return html;
