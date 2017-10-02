c5e3e7a5e867dbe4a378d41b70af6f1e16ca3874
Francisco M. Miranda <fmanuelos@gmail.com>
2017-10-02 10:05:27 -0400

    Eliminated the CDE Reference sub header

--- client/src/values-table/view.js
+++ client/src/values-table/view.js
@@ -1,30 +1,31 @@
 
-let tmpl = '<div class="container table-container"><div class="table-thead row">' +
-  '<div class="col-xs-3">' +
-    '<div class="table-th">Category / Node / Property</div>' +
-  '</div>' +
-  '<div class="col-xs-9">' +
-    '<div class="row">' +
-      '<div class="table-th table-header col-xs-6">GDC Values and Synonyms</div>' +
-      '<div class="table-th table-header col-xs-6">CDE references, permissible values and Synonyms</div>' +
-    '</div>' +
-    '<div class="row thead-row">' +
-      '<div class="table-th col-xs-3">Matched GDC Value</div>' +
-      '<div class="table-th col-xs-3">GDC Values: NCIt Synonyms</div>' +
-      '<div class="table-th table-header col-xs-6">CDE Values: NCIt Code and Synonyms</div>' +
-    '</div>' +
-  '</div>' +
-'</div>'+
-'<div class="row table-body" style="max-height: {{:mh}}px;"><div class="col-xs-12">{{for values}}' +
-'<div class="table-row row">' +
-  '<div class="table-td col-xs-3">'+
-      '{{:category}}<ul><li>{{:node}}<ul><li>{{:name}}</li></ul></li></ul>'+
-  '</div>'+
-  '<div class="col-xs-9 border-l"> {{for vs}}' +
-    '<div class="row {{if #getIndex() > 4}}row-toggle row-flex{{else}}row-flex{{/if}}" style="">' +
-      '<div class="table-td col-xs-3 border-r border-b">{{if n == "See All Values"}}<a href="javascript:getGDCData(\'{{:ref}}\',null);">See All Values</a>{{else}}<a href="javascript:getGDCData(\'{{:ref}}\',\'{{:n}}\');">{{if i_c !== undefined }}({{:i_c}}) {{else}}{{/if}}{{:n}}</a>{{/if}}</div>' +
-      '<div class="table-td col-xs-3 border-r border-b"><div class="row"><div class="col-xs-3">{{:n_c}}</div><div class="col-xs-9">{{for s}}{{:}}</br>{{/for}}</div></div></div>' +
-      '<div class="table-td col-xs-6 border-b">'
+let tmpl = '<div class="container table-container"><div class="table-thead row">'
+  +'<div class="col-xs-3">'
+    +'<div class="table-th">Category / Node / Property</div>'
+  +'</div>'
+  +'<div class="col-xs-9">'
+    +'<div class="row table-thead">'
+      +'<div class="col-xs-6">'
+        +'<div class="row"><div class="table-th table-header col-xs-12">GDC Values and Synonyms</div></div>'
+        +'<div class="row">'
+          +'<div class="table-th col-xs-6">Matched GDC Value</div>'
+          +'<div class="table-th col-xs-6">GDC Values: NCIt Synonyms</div>'
+        +'</div>'
+      +'</div>'
+      +'<div class="table-th table-header col-xs-6">CDE Values: NCIt Code and Synonyms</div>'
+    +'</div>'
+  +'</div>'
++'</div>'
++'<div class="row table-body" style="max-height: {{:mh}}px;"><div class="col-xs-12">{{for values}}'
++'<div class="table-row row">'
+  +'<div class="table-td col-xs-3">'
+      +'{{:category}}<ul><li>{{:node}}<ul><li>{{:name}}</li></ul></li></ul>'
+  +'</div>'
+  +'<div class="col-xs-9 border-l"> {{for vs}}'
+    +'<div class="row {{if #getIndex() > 4}}row-toggle row-flex{{else}}row-flex{{/if}}" style="">'
+      +'<div class="table-td col-xs-3 border-r border-b">{{if n == "See All Values"}}<a href="javascript:getGDCData(\'{{:ref}}\',null);">See All Values</a>{{else}}<a href="javascript:getGDCData(\'{{:ref}}\',\'{{:n}}\');">{{if i_c !== undefined }}({{:i_c}}) {{else}}{{/if}}{{:n}}</a>{{/if}}</div>'
+      +'<div class="table-td col-xs-3 border-r border-b"><div class="row"><div class="col-xs-3">{{:n_c}}</div><div class="col-xs-9">{{for s}}{{:}}</br>{{/for}}</div></div></div>'
+      +'<div class="table-td col-xs-6 border-b">'
         +'{{for cde_s}}'
         +'<div class="row">'
           +'<div class="col-xs-3">{{:c}}</div>'
