65b51fe3a67245505ef06c24d6a066517c16a7e1
Francisco M. Miranda <fmanuelos@gmail.com>
2017-10-03 15:16:57 -0400

    Fixed other bug in header

--- client/src/index.js
+++ client/src/index.js
@@ -12,12 +12,13 @@ $("#keywords").bind("input", func.suggest);
 
 $(document).on('click',func.removeBox);
 
-var heightSlider = $('.navbar').height();
+var heightSlider = $('.navbar .container').height();
+$('#body').attr('style', 'margin-top: '+ (heightSlider - 44) +'px !important');
 
-console.log(heightSlider);
-
-$('#body').attr('style', 'margin-top: '+ (heightSlider - 44) +'px !important');//.style('margin-top', (heightSlider - 64) + 'px', 'important');//.css({ 'margin-top' : (heightSlider - 64) + 'px' });
-
+$(window).resize(function() {
+  heightSlider = $('.navbar .container').height();
+  $('#body').attr('style', 'margin-top: '+ (heightSlider - 44) +'px !important');
+});
 
 function getGDCData(prop, target){
 	let uid = prop.replace(/@/g, '/');
