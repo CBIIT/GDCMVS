1fb50bc4bc393ffdd0dbe73670e9dc3d52e68ac2
Francisco M. Miranda <fmanuelos@gmail.com>
2018-06-07 14:25:31 -0400

    Change webpack to compress js and css files

--- webpack.config.js
+++ webpack.config.js
@@ -3,59 +3,54 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
-  entry: {
-    main: ['./client/src/index.js', './client/src/style.scss'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'client/static/dist')
-  },
-  module: {
-    rules: [
-
-      // JS 
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: [
-              ['env', {
-                targets: {
-                  browsers: ['> 1%', 'last 2 versions']
-                },
-                modules: false
-              }]
-            ]
+    entry: {
+      main: ['./client/src/index.js', './client/src/style.scss'],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'client/static/dist')
+    },
+    module: {
+        rules: [
+    
+          // JS 
+          { test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: { 
+                  presets: ['es2015','stage-2'] 
+                }
+            },
+          },
+    
+          // CSS
+          { test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: [
+                {
+                  loader: 'css-loader',
+                  options: {
+                    minimize: true
+                  }
+              }, 
+              'sass-loader']
+            })
           }
-        },
-        exclude: /node_modules/
+        ]
       },
-
-      // CSS
-      { test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            'sass-loader'
-          ]
+      plugins: [
+        new ExtractTextPlugin("styles.css"),
+        new FileManagerPlugin({
+        onEnd: [{
+            copy: [
+                { source: './client/views/body.html', destination: path.resolve(__dirname, 'gdc-docs/docs/Data_Dictionary/gdcmvs.md') },
+                { source: './client/static/dist/*', destination: path.resolve(__dirname, 'gdc-docs/theme/apps/gdcmvs/dist') },
+                { source: './client/static/lib/**/*', destination: path.resolve(__dirname, 'gdc-docs/theme/apps/gdcmvs/lib') }
+            ]
+            }]
         })
-      }
-    ]
-  },
-
-  //plugings
-  plugins: [
-    new ExtractTextPlugin('styles.css'),
-    new FileManagerPlugin({
-      onEnd: [{
-          copy: [
-            { source: './client/views/body.html', destination: path.resolve(__dirname, 'gdc-docs/docs/Data_Dictionary/gdcmvs.md') },
-            { source: './client/static/dist/*', destination: path.resolve(__dirname, 'gdc-docs/theme/apps/gdcmvs/dist') },
-            { source: './client/static/lib/**/*', destination: path.resolve(__dirname, 'gdc-docs/theme/apps/gdcmvs/lib') }
-          ]
-        }]
-    })
-  ]
-};
+      ],
+      mode: 'production'
+}
