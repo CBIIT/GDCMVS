77cebbfc25e365dc4e2c9684cb2a96035b1795aa
bhaumik55231 <bhaumik55231@gmail.com>
2018-06-25 16:11:02 -0400

    tabs-title and suggest item alert

--- client/src/tabs/view.js
+++ client/src/tabs/view.js
@@ -5,19 +5,19 @@ let tmpl = '<div class="results">' +
     '<div id="tab" class="btn-group tab-nav__group" data-toggle="buttons">' +
       '<ul class="tab-nav__ul" role="tablist">'+
       '<li role="presentation" class="tab-nav__li{{if vs_active}} active{{else}}{{/if}}">'+
-        '<div class="tab-nav__tooltip tooltip-target" data-toggle="tooltip" data-placement="bottom" data-delay=\'{"show":"1000"}\' data-trigger="hover focus" title="Text will be provided to inform users on how to interpret content of tabs.">'+
+        '<div class="tab-nav__tooltip tooltip-target" data-toggle="tooltip" data-placement="bottom" data-delay=\'{"show":"1000"}\' data-trigger="hover focus" title="For a search term, the values tab displays matches among the accepted values for GDC metadata properties. These values will pass validation when submitted under the corresponding property.">'+
           '<a id="tab-values" href="#values" class="tab-nav__btn" aria-controls="values" role="tab" data-toggle="tab" aria-expanded="true">Values</a>'+
           '<span class="tab-nav__notification">{{:vs_len}}</span>'+
         '</div>'+
       '</li>'+
       '<li role="presentation" class="tab-nav__li{{if ps_active}} active{{else}}{{/if}}">'+
-        '<div class="tab-nav__tooltip tooltip-target" data-toggle="tooltip" data-placement="bottom" data-delay=\'{"show":"1000"}\' data-trigger="hover focus" title="Text will be provided to inform users on how to interpret content of tabs.">'+
+        '<div class="tab-nav__tooltip tooltip-target" data-toggle="tooltip" data-placement="bottom" data-delay=\'{"show":"1000"}\' data-trigger="hover focus" title="For a search term, the properties tab displays matches among valid GDC metadata properties. These can be thought of as the valid column headings that accept values related to the case or other object being described.">'+
           '<a id="tab-properties" href="#properties" class="tab-nav__btn" aria-controls="properties" role="tab" data-toggle="tab" aria-expanded="true">Properties</a>'+
           '<span class="tab-nav__notification">{{:ps_len}}</span>'+
         '</div>'+
       '</li>'+
       '<li role="presentation" class="tab-nav__li{{if trs_active}} active{{else}}{{/if}}">'+
-        '<div class="tab-nav__tooltip tooltip-target" data-toggle="tooltip" data-placement="bottom" data-delay=\'{"show":"1000"}\' data-trigger="hover focus" title="Text will be provided to inform users on how to interpret content of tabs.">'+
+        '<div class="tab-nav__tooltip tooltip-target" data-toggle="tooltip" data-placement="bottom" data-delay=\'{"show":"1000"}\' data-trigger="hover focus" title="For a search term, this tab displays relevant matches in the context of the GDC Dictionary. The Dictionary defines the property names, the sets of accepted values for those properties, and contains readable definitions, links, and other information to aid users in understanding the vocabular.">'+
           '<a id="tab-dictionary" href="#dictionary" class="tab-nav__btn" aria-controls="dictionary" role="tab" data-toggle="tab" aria-expanded="true">Dictionary</a>'+
           '<span class="tab-nav__notification">{{:trs_len}}</span>'+
         '</div>'+
