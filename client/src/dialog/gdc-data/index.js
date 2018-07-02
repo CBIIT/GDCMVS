44d29aedd8a890e2e080368a19f2ccfd7c594f94
bhaumik55231 <bhaumik55231@gmail.com>
2018-07-02 13:52:20 -0400

    pop up bug fix

--- client/src/dialog/gdc-data/index.js
+++ client/src/dialog/gdc-data/index.js
@@ -11,15 +11,29 @@ export default function gdcData(prop, item) {
 
     let windowEl = $(window);
     let icdo = false;
+    let new_item = [];
     items.forEach(function(item){
       if(item.i_c !== undefined){
         icdo = true;
+        new_item.push(item);
       }
     });
-
+    //console.log(items);
     let target = item == undefined ? item : item.replace(/<b>/g,"").replace(/<\/b>/g, "");
-    let header = $.templates(tmpl.header).render({target: target, icdo: icdo, items_length: items.length});
-    let html = $.templates(tmpl.body).render({target: target, icdo: icdo, items: items});
+    let html = "";
+    let header = "";
+    if(icdo){
+      header = $.templates(tmpl.header).render({target: target, icdo: icdo, items_length: new_item.length});
+      html = $.templates(tmpl.body).render({target: target, icdo: icdo, items: new_item});
+    }else{
+      header = $.templates(tmpl.header).render({target: target, icdo: icdo, items_length: items.length});
+      html = $.templates(tmpl.body).render({target: target, icdo: icdo, items: items});
+
+    }
+
+
+
+
     let tp = (window.innerHeight * 0.2 < shared.headerOffset() )? 20 : window.innerHeight * 0.2;
 
     //display result in a table
