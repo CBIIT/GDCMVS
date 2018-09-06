4854bbb392eb62e03954505d669b3b127e5ccb32
bhaumik55231 <bhaumik55231@gmail.com>
2018-09-06 16:25:21 -0400

    Bug Fix : - Sorry, no results found for keyword: 800

--- routes.js
+++ routes.js
@@ -6,18 +6,18 @@
 
 var config = require('./server/config');
 var compression = require('compression');
-var helmet =  require('helmet');
+var helmet = require('helmet');
 
-module.exports = function(app){
+module.exports = function (app) {
 	app.use(compression());
 	app.use(helmet());
 	//allows CrossDomainAccess to API
-	app.use(function(req, res, next){
+	app.use(function (req, res, next) {
 		res.header('Access-Control-Allow-Origin', '*');
 		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-		if(next){
+		if (next) {
 			next();
 		}
 	});
@@ -25,12 +25,12 @@ module.exports = function(app){
 	app.use('/search/', require('./server/service/search'));
 
 	//put all the routers here
-	app.get('/', function(req, res){
+	app.get('/', function (req, res) {
 		res.status(200).render(app.get('views') + '/index.html');
 	});
 
 	// All other routes should redirect to error page
-  app.get('*', function(req, res) {
+	app.get('*', function (req, res) {
 		res.status(404).sendFile(app.get('views') + '/404.html');
-  });
+	});
 };
