20f3494487ab5b18383d478c344e4dfea708cd47
Francisco M. Miranda <fmanuelos@gmail.com>
2018-09-10 13:13:21 -0400

    Changes in notification errors

--- client/src/api.js
+++ client/src/api.js
@@ -13,35 +13,35 @@ const api = {
     $.getJSON(baseUrl + '/all/p', {keyword:keyword, option: JSON.stringify(option)}, function(result) {
         callback(keyword, option, result);
       }).fail(function(xhr, textStatus, errorThrown){
-        error();
+        error(xhr.status, errorThrown);
       });
   },
   getGDCDataById(id, callback, error){
     $.getJSON(baseUrl + '/p/local/vs', {id:id}, function(result){
         callback(id,result);
       }).fail(function(xhr, textStatus, errorThrown){
-        error();
+        error(xhr.status, errorThrown);
       });
   },
   getCDEDataById(id, callback, error){
     $.getJSON(baseUrl + '/p/cde/vs', {id:id}, function(result){
         callback(id,result);
       }).fail(function(xhr, textStatus, errorThrown){
-        error();
+        error(xhr.status, errorThrown);
       });
   },
   getGDCandCDEDataById(ids, callback, error){
     $.getJSON(baseUrl + '/p/both/vs', {local:ids.local, cde: ids.cde}, function(result){
         callback(ids,result);
       }).fail(function(xhr, textStatus, errorThrown){
-        error();
+        error(xhr.status, errorThrown);
       });
   },
   evsRestApi(id, callback, error){
     $.getJSON(baseUrl + '/ncit/detail?code=' + id, function(result){
       callback(id,result);
-    }).fail(function(){
-      error();
+    }).fail(function(xhr, textStatus, errorThrown){
+      error(xhr.status, errorThrown);
     });
   }
 }
