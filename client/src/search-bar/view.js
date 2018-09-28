c8a8c5eb22b6cf365f5f167823a1b93c9891e5c2
Francisco M. Miranda <fmanuelos@gmail.com>
2018-09-28 16:06:08 -0400

    New structure for search-bar files

--- client/src/search-bar/view.js
+++
@@ -1,7 +0,0 @@
-
-let tmpl = '{{for options}}<div class="suggest__object">'
-			+'<span class="suggest__name">{{:id}}</span>'
-			+'<label class="suggest__type">{{:type}}</label>'
-			+'</div>{{/for}}';
-
-export default tmpl;
