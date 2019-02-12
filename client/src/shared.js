d938f5acd8cbfbaf027fb94f9aa5ffcea689ab84
Francisco M. Miranda <fmanuelos@gmail.com>
2019-02-12 13:07:00 -0500

    Fix draggable space for popups

--- client/src/shared.js
+++ client/src/shared.js
@@ -18,15 +18,16 @@ export const onScroll = ($window) => {
   }
 }
 
-export const onResize = ($docsContainer, $parentContainer) => {
+export const onResize = ($docsContainer, $parentContainer, $mainContainer) => {
   headerHeight = $('.navbar .container').height();
   setHeight($docsContainer, $parentContainer);
   headerOffset = headerHeight;
 }
 
-export const setHeight = ($docsContainer, $parentContainer) => {
+export const setHeight = ($docsContainer, $parentContainer, $mainContainer) => {
   $docsContainer.attr('style', 'margin-top: ' + (headerHeight - 54) + 'px !important');
   $parentContainer.attr('style', 'min-height: calc(100vh - ' + (headerHeight + 10) + 'px)');
+  $mainContainer.attr('style', 'min-height: calc(100vh - ' + (headerHeight + 12) + 'px)');
 }
 
 export const errorNotification = (status, errorThrown) => {
