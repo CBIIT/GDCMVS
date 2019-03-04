2dfa7f8cf82830f50ff7ed6a278ac156c5995bd2
Francisco M. Miranda <fmanuelos@gmail.com>
2019-03-04 15:28:49 -0500

    Fix bug table break words

--- client/src/dialog/gdc-terms/gdc-terms.js
+++ client/src/dialog/gdc-terms/gdc-terms.js
@@ -318,7 +318,7 @@ const templateList = (items, icdo, keyword = 'default') => {
                 </thead>
                 ${n_syn.s.map((s) =>`
                   <tr>
-                    <td>${s.termName}</td>
+                    <td><p class="table_td-term">${s.termName}<p></td>
                     <td>${s.termSource !== undefined && s.termSource !== null ? s.termSource : ``}</td>
                     <td>${s.termGroup !== undefined && s.termGroup !== null ? s.termGroup : ``}</td>
                   </tr>
@@ -345,7 +345,7 @@ const templateList = (items, icdo, keyword = 'default') => {
                   </thead>
                   ${item.s.map((s) =>`
                     <tr>
-                      <td>${s.termName}</td>
+                      <td><p class="table_td-term">${s.termName}</p></td>
                       <td>${s.termSource !== undefined && s.termSource !== null ? s.termSource : ``}</td>
                       <td>${s.termGroup !== undefined && s.termGroup !== null ? s.termGroup : ``}</td>
                     </tr>
@@ -377,7 +377,7 @@ const templateList = (items, icdo, keyword = 'default') => {
                   </thead>
                   ${n_syn.s.map((s) =>`
                     <tr>
-                      <td>${s.termName}</td>
+                      <td><p class="table_td-term">${s.termName}</p></td>
                       <td>${s.termSource !== undefined && s.termSource !== null ? s.termSource : ``}</td>
                       <td>${s.termGroup !== undefined && s.termGroup !== null ? s.termGroup : ``}</td>
                     </tr>
@@ -404,7 +404,7 @@ const templateList = (items, icdo, keyword = 'default') => {
                   </thead>
                   ${item.s.map((s) =>`
                     <tr>
-                      <td>${s.termName}</td>
+                      <td><p class="table_td-term">${s.termName}</p></td>
                       <td>${s.termSource !== undefined && s.termSource !== null ? s.termSource : ``}</td>
                       <td>${s.termGroup !== undefined && s.termGroup !== null ? s.termGroup : ``}</td>
                     </tr>
