3385ceeed5b1e5c3a51372c25a7e401b8795a694
bhaumik55231 <bhaumik55231@gmail.com>
2019-04-20 16:54:05 -0400

    Impleminting new front end design

--- client/src/props-table/props-table.js
+++ client/src/props-table/props-table.js
@@ -31,15 +31,24 @@ export default (items, keyword, search_option) => {
         });
       }
 
+      let highlight_cdeId = ("cde.id" in highlight) ? highlight["cde.id"] : undefined;
+      let highlight_cdeId_obj = {};
+      if(highlight_cdeId !== undefined){
+        highlight_cdeId.forEach(val => {
+          let tmp = val.replace(/<b>/g, "").replace(/<\/b>/g, "");
+          if(highlight_cdeId_obj[tmp] === undefined) highlight_cdeId_obj[tmp] = val;
+        });
+      }
+
       let prop_obj = {};
       prop_obj.category = source.category;
       prop_obj.node = source.node;
       prop_obj.property = highlight_property_obj[source.property] ? highlight_property_obj[source.property] : source.property;
       prop_obj.property_desc = highlight_property_desc_obj[source.property] ? highlight_property_desc_obj[source.property] : source.property_desc;
-      if(source.type !== undefined && source.type !== "enum") prop_obj.type = source.type;
+      if(source.type !== undefined && source.type !== "" && source.type !== "enum") prop_obj.type = source.type;
       if(source.enum !== undefined) prop_obj.enum = source.enum;
       if(source.cde !== undefined){
-        prop_obj.cdeId = source.cde.id;
+        prop_obj.cdeId = highlight_cdeId_obj[source.cde.id] ? highlight_cdeId_obj[source.cde.id] : source.cde.id;
         prop_obj.cdeUrl = source.cde.url;
       }
       props.push(prop_obj);
