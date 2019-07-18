2fe1409e7175e80983277801a78c2e60cba814b9
Francisco M. Miranda <fmanuelos@gmail.com>
2019-07-18 11:19:39 -0400

    Update GDC dictionary version qq-rc2

--- server/service/api/index.js
+++ server/service/api/index.js
@@ -2,8 +2,13 @@
 
 const express = require('express');
 const controller = require('./../search/search.controller');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../config/swagger');
 
 const router = express.Router();
+
+router.use('/docs', swaggerUi.serve);
+router.get('/docs', swaggerUi.setup(swaggerDocument));
 
 router.get('/search', controller.searchAPI);
 
