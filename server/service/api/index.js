6616998112e7362556e0b165952fd6ff40dd6f88
Francisco M. Miranda <fmanuelos@gmail.com>
2019-08-21 14:03:23 -0400

    Dynamic swagger documentation

--- server/service/api/index.js
+++ server/service/api/index.js
@@ -7,8 +7,24 @@ const swaggerDocument = require('../../config/swagger');
 
 const router = express.Router();
 
-router.use('/docs', swaggerUi.serve);
-router.get('/docs', swaggerUi.setup(swaggerDocument));
+router.get('/', function (req, res) {
+  res.json({
+    'version': '1.3.0',
+    'title': 'GDCMVS Rest API',
+    'description': req.protocol + '://' + req.headers.host + req.baseUrl,
+    'documentation': req.protocol + '://' + req.headers.host + req.baseUrl + '/docs/'
+  });
+});
+
+router.use('/docs',
+  swaggerUi.serve,
+  function (req, res) {
+    const protocol = req.protocol;
+    const host = req.get('host');
+    const baseUrl = req.baseUrl !== undefined ? req.baseUrl.replace(/\/docs/g, '') : '';
+    swaggerUi.setup(swaggerDocument(protocol, host, baseUrl))(req, res);
+  }
+);
 
 router.get('/search', controller.searchAPI);
 
