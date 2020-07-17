d325bd5de9592254c7c2d4ffc6d8d578a07daf69
Francisco M. Miranda <fmanuelos@gmail.com>
2020-07-17 13:27:00 -0400

    Bug Fix: webpack files

--- webpack/webpack.docs.config.js
+++ webpack/webpack.docs.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: ['./client/src/index.js', './client/src/style.css'],
+    main: ['./client/src/index.js', './client/src/style.css']
   },
   output: {
     filename: 'bundle.js',
@@ -15,39 +15,39 @@ module.exports = {
   module: {
     rules: [
 
-      // JS 
+      // JS
       { test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: { 
-              presets: ['@babel/preset-env']
-            }
-        },
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
 
       // CSS
       { test: /\.css$/,
         use: [
-          { loader: "style-loader" },
+          { loader: 'style-loader' },
           MiniCssExtractPlugin.loader,
-          { loader: "css-loader" },
+          { loader: 'css-loader' }
         ]
       }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({filename: "styles.css"}),
+    new MiniCssExtractPlugin({ filename: 'styles.css' }),
     new OptimizeCssAssetsPlugin(),
     new CleanWebpackPlugin(),
     new FileManagerPlugin({
-    onEnd: [{
+      onEnd: [{
         copy: [
-            { source: './client/views/body.html', destination: path.resolve(__dirname, '../gdc-docs/docs/Data_Dictionary/gdcmvs.md') },
-            { source: './client/static/dist/*.{js,css}', destination: path.resolve(__dirname, '../gdc-docs/theme/apps/gdcmvs/dist') },
-            { source: './client/static/lib/**/*', destination: path.resolve(__dirname, '../gdc-docs/theme/apps/gdcmvs/lib') }
+          { source: './client/views/body.html', destination: path.resolve(__dirname, '../gdc-docs/docs/Data_Dictionary/gdcmvs.md') },
+          { source: './client/static/dist/*.{js,css}', destination: path.resolve(__dirname, '../gdc-docs/theme/apps/gdcmvs/dist') },
+          { source: './client/static/lib/**/*', destination: path.resolve(__dirname, '../gdc-docs/theme/apps/gdcmvs/lib') }
         ]
-        }]
+      }]
     })
   ]
-}
+};
--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: ['./client/src/index.js', './client/src/style.css'],
+    main: ['./client/src/index.js', './client/src/style.css']
   },
   output: {
     filename: 'bundle.js',
@@ -14,30 +14,30 @@ module.exports = {
   module: {
     rules: [
 
-      // JS 
+      // JS
       { test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: { 
-              presets: ['@babel/preset-env']
-            }
-        },
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
 
       // CSS
       { test: /\.css$/,
         use: [
-          { loader: "style-loader" },
+          { loader: 'style-loader' },
           MiniCssExtractPlugin.loader,
-          { loader: "css-loader" },
+          { loader: 'css-loader' }
         ]
-      },
+      }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({filename: "styles.css"}),
+    new MiniCssExtractPlugin({ filename: 'styles.css' }),
     new OptimizeCssAssetsPlugin(),
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin()
   ]
-}
+};
