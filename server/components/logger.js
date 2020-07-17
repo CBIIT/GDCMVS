1b5925001158e7e22c06e96fb64bb1d8fc41abb9
Francisco M. Miranda <fmanuelos@gmail.com>
2020-07-17 13:07:55 -0400

    Update npm libraries and fix some bug

--- server/components/logger.js
+++ server/components/logger.js
@@ -2,8 +2,6 @@
  * back-end logger for application
  */
 
-'use strict';
-
 const winston = require('winston');
 const config = require('../config');
 winston.emitErrs = true;
@@ -11,22 +9,22 @@ winston.emitErrs = true;
 const tsFormat = () => (new Date()).toLocaleTimeString();
 
 var logger = new winston.Logger({
-    transports: [
-	    new (require('winston-daily-rotate-file'))({
-	      filename: config.logDir + '/-warning.log',
-	      timestamp: tsFormat,
-	      datePattern: 'yyyy-MM-dd',
-	      prepend: true,
-	      level: 'warn'
-	    }),
-        new winston.transports.Console({
-            level: 'debug',
-            handleExceptions: true,
-            json: false,
-            colorize: true
-        })
-    ],
-    exitOnError: false
+  transports: [
+    new (require('winston-daily-rotate-file'))({
+      filename: config.logDir + '/-warning.log',
+      timestamp: tsFormat,
+      datePattern: 'yyyy-MM-dd',
+      prepend: true,
+      level: 'warn'
+    }),
+    new winston.transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+      json: false,
+      colorize: true
+    })
+  ],
+  exitOnError: false
 });
 
 module.exports = logger;
