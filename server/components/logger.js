bfe679efae7d99923a67d70cabe4a536da263e91
Francisco M. Miranda <fmanuelos@gmail.com>
2025-02-05 12:45:29 -0500

    Update to winston 3

--- server/components/logger.js
+++ server/components/logger.js
@@ -2,29 +2,43 @@
  * back-end logger for application
  */
 
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, printf, colorize } = format;
+const DailyRotateFile = require('winston-daily-rotate-file');
 const config = require('../config');
-winston.emitErrs = true;
 
-const tsFormat = () => (new Date()).toLocaleTimeString();
-
-var logger = new winston.Logger({
-  transports: [
-    new (require('winston-daily-rotate-file'))({
-      filename: config.logDir + '/-warning.log',
-      timestamp: tsFormat,
-      datePattern: 'yyyy-MM-dd',
-      prepend: true,
-      level: 'warn'
-    }),
-    new winston.transports.Console({
-      level: 'debug',
-      handleExceptions: true,
-      json: false,
-      colorize: true
-    })
-  ],
-  exitOnError: false
+// Define the custom format for the logs
+const customFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}]: ${message}`;
 });
 
+// Create the logger
+const logger = createLogger({
+  // Set the default log level
+  level: 'warn',
+  format: combine(
+    timestamp(),
+    colorize(), // Colorize the output
+    customFormat
+  ),
+  transports: [
+    new transports.Console(
+      {
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true
+      }
+    ), // Log to the console
+    new DailyRotateFile({
+      level: 'warn',
+      filename: 'mvs-%DATE%.log', // Log file name pattern
+      dirname: config.logDir, // Directory where log files are stored
+      datePattern: 'YYYY-MM-DD', // Date pattern for rotating logs
+      maxFiles: '14d' // Keep logs for 14 days
+    })
+  ]
+});
+
+// Export the logger
 module.exports = logger;
